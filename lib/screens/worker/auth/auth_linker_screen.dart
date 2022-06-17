adc197bff4247e22ec6576cc32035cee52633e49
Robert-Doe <robertdoe60@gmail.com>
2022-06-17 11:17:49 +0000

    Addition of Client Map screen

---
+++ lib/screens/worker/auth/auth_linker_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthLinkerScreen extends StatefulWidget {
+  const AuthLinkerScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthLinkerScreenState createState() => _AuthLinkerScreenState();
+}
+
+class _AuthLinkerScreenState extends State<AuthLinkerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
